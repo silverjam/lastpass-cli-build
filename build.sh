05fadc1d7dcc8f8b7874a907d1314b4568e061ca
Jason Mobarak <jason@swiftnav.com>
2019-12-09 10:17:32 -0800

    Use lastpass-cli dir

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
+
+set -x
+
 docker build -t lpass  .
-docker run -v $PWD:/host lpass sh -c "cp /work/build/*.deb /host"
+
+docker run \
+  -v "$PWD:/host" lpass sh -c 'cp /work/lastpass-cli/build/*.deb /host'
